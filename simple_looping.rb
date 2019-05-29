ed226ce12f8f712e438f4b1b1dec440b047058fc
AN K <a2nkale@gmail.com>
2019-05-29 21:19:45 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,8 @@ end
 end
 
 def for_iterator(number_of_times)
- fpr counter in 1..10 
+ fpr counter in 1..10 do 
+   puts "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
