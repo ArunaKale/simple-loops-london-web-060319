8ceb6cfc2e4e75603f520a06d02dbf259597ab2c
AN K <a2nkale@gmail.com>
2019-05-29 21:16:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,6 +25,7 @@ def while_iterator(number_of_times)
   while counter < number_of_times
   puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
+end 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 end
 
